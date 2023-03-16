943140eb8e2e09187a2a58efda44ac49d2b27c19
VlikToldo <108451651+VlikToldo@users.noreply.github.com>
2023-03-16 17:01:38 +0300

    Update ImageFinder.jsx

--- src/components/modules/ImageFinder/ImageFinder.jsx
+++ src/components/modules/ImageFinder/ImageFinder.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect} from 'react';
 import { ColorRing } from 'react-loader-spinner';
 
 import Searchbar from './Searchbar/Searchbar';
